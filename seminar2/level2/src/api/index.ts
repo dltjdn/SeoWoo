31d6ea9f6272cf895ad4b44815c9560f106a3505
dltjdn <hana40004@naver.com>
2022-04-19 02:19:29 +0900

    seminar2 과제 수정

--- seminar2/level2/src/api/index.ts
+++ seminar2/level2/src/api/index.ts
@@ -2,9 +2,20 @@ import express, {Router} from 'express';
 
 const router: Router = express.Router();
 
-router.use('/user', require('./user'));
-router.use('/like', require('./like'));
-router.use('/blog', require('./blog'));
-router.use('/signup', require('./signup'));
+// router.use('/user', require('./user'));
+// router.use('/like', require('./like'));
+// router.use('/blog', require('./blog'));
+// router.use('/signup', require('./signup'));
+
+const userRouter = require('./user');
+const blogRouter = require('./blog');
+const signupRouter = require('./signup');
+const likeRouter= require('./like');
+
+router.use('/user', userRouter);
+router.use('/blog', blogRouter);
+router.use('/signup', signupRouter);
+router.use('/like', likeRouter);
+
 
 module.exports = router;
