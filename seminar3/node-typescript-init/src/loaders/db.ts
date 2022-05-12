50022ffad6ae38b4e0f01115cfbab6bba67bfa45
dltjdn <hana40004@naver.com>
2022-05-12 21:02:45 +0900

    [Remove] seminar3 Movie, Review in db.ts

--- seminar3/node-typescript-init/src/loaders/db.ts
+++ seminar3/node-typescript-init/src/loaders/db.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import config from "../config";
-import Movie from "../models/Movie";
-import Review from "../models/Review";
+
 
 const connectDB = async () => {
     try {
@@ -11,13 +10,7 @@ const connectDB = async () => {
 
         console.log("Mongoose Connected ...");
 
-        Movie.createCollection().then(function(collection){
-            console.log("Movie collection created");
-        });
-
-        Review.createCollection().then(function(collection){
-            console.log("Review Collection Created");
-        })
+    
     } catch (err: any) {
         
         console.error(err.message);
