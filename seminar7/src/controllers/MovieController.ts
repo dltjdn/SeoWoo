2abd5f31fce4ad3cad209515fd7a70214a08b32b
Dltjdn <hana400004@naver.com>
2022-06-03 17:06:51 +0900

    [CHORE] 영화 검색 발전 과제

--- seminar7/src/controllers/MovieController.ts
+++ seminar7/src/controllers/MovieController.ts
@@ -116,26 +116,37 @@ const updateMovieComment = async (req: Request, res: Response) => {
  * @access Public
  */
 const getMovieBySearch = async (req: Request, res: Response) => {
-    const { search, option } = req.query;
-
-    const isOptionType = (option: string): option is MovieOptionType => {
-        return ["title", "director", "title_director"].indexOf(option) !== -1;
-    }
-
-    if (!isOptionType(option as string)) {
-        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
-    }
 
     const page: number = Number(req.query.page || 1);
 
-    try {
-        const data = await MovieService.getMovieBySearch(search as string, option as MovieOptionType, page);
+    if (req.query.search && req.query.option) {
+        const { search, option } = req.query;
+        const isOptionType = (option: string): option is MovieOptionType => {
+            return ["title", "director", "title_director"].indexOf(option) !== -1;
+        }
+        if (!isOptionType(option as string)) {
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+        }
 
-        res.status(statusCode.OK).send(util.success(statusCode.OK, message.SEARCH_MOVIE_SUCCESS, data));
-    } catch (error) {
-        console.log(error);
-        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR))
+        try {
+            const data = await MovieService.getMovieBySearch(page, search as string, option as MovieOptionType);
+            res.status(statusCode.OK).send(util.success(statusCode.OK, message.SEARCH_MOVIE_SUCCESS, data));
+        } catch (error) {
+            console.log(error);
+            res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR))
+        }
     }
+    else {
+        try {
+            const data = await MovieService.getMovieBySearch(page);
+            res.status(statusCode.OK).send(util.success(statusCode.OK, message.SEARCH_MOVIE_SUCCESS, data));
+        } catch (error) {
+            console.log(error);
+            res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+        }
+
+    }
+
 }
 
 export default {
--- seminar7/src/services/MovieService.ts
+++ seminar7/src/services/MovieService.ts
@@ -72,6 +72,7 @@ const updateMovieComment = async (movieId: string, commentId: string, userId: st
                     'comments.$.writer': userId,
                     'comments.$.comment': movieCommentUpdateDto.comment
                 }
+
             },
             { new: true }
         );
@@ -84,51 +85,58 @@ const updateMovieComment = async (movieId: string, commentId: string, userId: st
     }
 }
 
-const getMovieBySearch = async (search: string, option: MovieOptionType, page: number): Promise<MoviesResponseDto> => {
-    const regex = (pattern: string) => new RegExp(`.*${pattern}.*`);
+const getMovieBySearch = async (page: number, search?: string, option?: MovieOptionType): Promise<MoviesResponseDto> => {
+
     try {
-        const titleRegex: RegExp = regex(search);
-
-        let movies: MovieInfo[] = [];
         const perPage: number = 2;
+        let movies: MovieInfo[] = [];
 
+        if (search && option) {
+            const regex = (pattern: string) => new RegExp(`.*${pattern}.*`);
+            const titleRegex: RegExp = regex(search);
 
-        if (option === 'title') {
-            movies = await Movie.find({ title: { $regex: titleRegex } })
-                .sort({ createdAt: -1 })
-                .skip(perPage * (page - 1))
-                .limit(perPage);
-        } else if (option === 'director') {
-            movies = await Movie.find({ director: { $regex: titleRegex } })
-                .sort({ createdAt: -1 })
-                .skip(perPage * (page - 1))
-                .limit(perPage);
+            if (option === 'title') {
+                movies = await Movie.find({ title: { $regex: titleRegex } })
+                    .sort({ createdAt: -1 })
+                    .skip(perPage * (page - 1))
+                    .limit(perPage);
+            } else if (option === 'director') {
+                movies = await Movie.find({ director: { $regex: titleRegex } })
+                    .sort({ createdAt: -1 })
+                    .skip(perPage * (page - 1))
+                    .limit(perPage);
+            } else {
+                movies = await Movie.find({
+
+                    $or: [
+                        { title: { $regex: titleRegex } },
+                        { director: { $regex: titleRegex } }
+                    ]
+                })
+                    .sort({ createdAt: -1 })
+                    .skip(perPage * (page - 1))
+                    .limit(perPage);
+            }
         } else {
-            movies = await Movie.find({
-                $or: [
-                    { title: { $regex: titleRegex } },
-                    { director: { $regex: titleRegex } }
-                ]
-            })
+            movies = await Movie.find({})
                 .sort({ createdAt: -1 })
                 .skip(perPage * (page - 1))
                 .limit(perPage);
-        }
 
+        }
         const total: number = await Movie.countDocuments({});
         const lastPage: number = Math.ceil(total / perPage);
-
         const data = {
             movies,
             lastPage
         }
-
         return data;
 
     } catch (error) {
         console.log(error);
         throw error;
     }
+
 }
 
 export default {
